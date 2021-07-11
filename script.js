f20ae04e9ab605699abe6b02d059edd5e2751b5c
Aashish Dhakal <ashish.dhakal07@gmail.com>
2021-07-11 17:32:29 +0545

    My first commit for Shweta Wedding Project

---
+++ script.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById("menu");
+
+            function showMenu(){
+                menu.style.right = "0";
+            }
+
+            function hideMenu(){
+                menu.style.right = "-40vw";
+            }
